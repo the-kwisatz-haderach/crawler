c4f5d37e52fa019327caf66ee006c149fca4a97f
the-kwisatz-haderach <gustaflundstrom90@gmail.com>
2023-02-06 13:01:01 +0100

    Fix

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,11 @@ import SiteCrawler from './SiteCrawler'
 import { saveToDb } from './crawlers/common'
 import { osmrtnicaObitCategoryProcessor } from './crawlers/osmrtnica'
 
-// new SiteCrawler({
-//   url: 'https://www.osmrtnica.ba/category/obavijest-o-smrti/',
-//   documentProcessor: osmrtnicaObitCategoryProcessor,
-//   outputHandler: saveToDb('osmrtnica')
-// }).crawl({
-//   headless: process.env.PUPPETEER_HEADLESS !== 'false',
-//   slowMo: 100
-// })
-
-console.log('HELLO!')
+new SiteCrawler({
+  url: 'https://www.osmrtnica.ba/category/obavijest-o-smrti/',
+  documentProcessor: osmrtnicaObitCategoryProcessor,
+  outputHandler: saveToDb('osmrtnica')
+}).crawl({
+  headless: process.env.PUPPETEER_HEADLESS !== 'false',
+  slowMo: 100
+})
