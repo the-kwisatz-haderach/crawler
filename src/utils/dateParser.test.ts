90f080fd90ac803a9e310e86013eddf0223923c3
the-kwisatz-haderach <gustaflundstrom90@gmail.com>
2023-02-07 10:16:02 +0100

    Upd test.

--- src/utils/dateParser.test.ts
+++ src/utils/dateParser.test.ts
@@ -2,7 +2,7 @@ import { dateParser } from './dateParser'
 
 describe('dateParser', () => {
   it('parses a single year', () => {
-    expect(dateParser('1934')).toEqual('1934-01-01T00:00:00.000Z')
+    expect(dateParser('1934.')).toEqual('1934-01-01T00:00:00.000Z')
   })
   it('parses a full date string', () => {
     expect(dateParser('4.2.2023.')).toEqual('2023-02-04T00:00:00.000Z')
