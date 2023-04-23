77ab093b010beaecf37e7f03a774e08e2370e338
the-kwisatz-haderach <gustaflundstrom90@gmail.com>
2023-04-23 11:29:49 +0200

    Upd

--- src/index.ts
+++ src/index.ts
@@ -8,26 +8,34 @@ const crawlOptions: PuppeteerLaunchOptions = {
   slowMo: Number.parseInt(process.env.PUPPETEER_SLOWMO || '100')
 }
 
-new SiteCrawler({
+const crawler1 = new SiteCrawler({
   url: 'https://www.osmrtnica.ba/category/obavijest-o-smrti/',
   documentProcessor: processors.osmrtnicaObitCategoryProcessor,
   outputHandler: saveToDb('osmrtnica')
-}).crawl(crawlOptions)
+})
 
-new SiteCrawler({
+const crawler2 = new SiteCrawler({
   url: 'https://www.osmrtnica.ba/category/sjecanje/',
   documentProcessor: processors.sjecanjeObitCategoryProcessor,
   outputHandler: saveToDb('sjecanje')
-}).crawl(crawlOptions)
+})
 
-new SiteCrawler({
+const crawler3 = new SiteCrawler({
   url: 'https://www.osmrtnica.ba/category/posljednji-pozdrav/',
   documentProcessor: processors.posljednjiPozdravObitCategoryProcessor,
   outputHandler: saveToDb('posljednji-pozdrav')
-}).crawl(crawlOptions)
+})
 
-new SiteCrawler({
+const crawler4 = new SiteCrawler({
   url: 'https://www.osmrtnica.ba/category/posljednje-zbogom/',
   documentProcessor: processors.zbogomObitCategoryProcessor,
   outputHandler: saveToDb('zbogom')
-}).crawl(crawlOptions)
+})
+
+crawler1.crawl(crawlOptions).finally(() => {
+  crawler2.crawl(crawlOptions).finally(() => {
+    crawler3.crawl(crawlOptions).finally(() => {
+      crawler4.crawl(crawlOptions)
+    })
+  })
+})
